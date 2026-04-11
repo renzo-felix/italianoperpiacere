e989df1c04e510ed04c2028cea984f70f17c3a0c
RenzoFelix <renzo@reditorial.es>
2026-04-11 08:30:47 -0500

    adda

--- scraper_transcripciones.py
+++ scraper_transcripciones.py
@@ -138,17 +138,17 @@ def process_modulo(modulo_num: int, titulo: str, url: str):
 # Formato: (numero_modulo, "Titulo", "URL_YouTube")
 # =====================================================
 MODULOS = [
-    (1,  "Di dove sei?",               "https://www.youtube.com/watch?v=tDESxMneCuM"),
-(2,  "Una notte a Roma",           "https://youtu.be/nJ0gKWX8mjM"),
-(3,  "Che ore sono?",              "https://www.youtube.com/watch?v=7mi5_TnIORo"),
-(4,  "Che lavoro fai?",            "https://youtu.be/VsEBjpQTlyY"),
-(5,  "Al bar",                     "https://youtu.be/2_VwcAlQUyU"),
-(6,  "La mia giornata",            "https://www.youtube.com/watch?v=-ECZVRh6sgk"),
-(7,  "In famiglia",                "https://youtu.be/mvl1hKoWWdM"),
-(8,  "In treno o in macchina?",    "https://youtu.be/nmtMdxkKHUo"),
-(9,  "Mi piace moltissimo!",       "https://youtu.be/7RXiaCyz7O8"),
-(10, "Il concerto è andato bene!", "https://youtu.be/FpOEN93LX-E"),  
-(11, "Ieri sera",                  "https://youtu.be/9A5gTgcgQ-I"),
+    (1,  "Di dove sei?",               "https://youtu.be/s7MBSo0E71o?si=rjFi6Lvw0J5IpOrK"),
+# (2,  "Una notte a Roma",           "https://youtu.be/nJ0gKWX8mjM"),
+# (3,  "Che ore sono?",              "https://www.youtube.com/watch?v=7mi5_TnIORo"),
+# (4,  "Che lavoro fai?",            "https://youtu.be/VsEBjpQTlyY"),
+# (5,  "Al bar",                     "https://youtu.be/2_VwcAlQUyU"),
+# (6,  "La mia giornata",            "https://www.youtube.com/watch?v=-ECZVRh6sgk"),
+# (7,  "In famiglia",                "https://youtu.be/mvl1hKoWWdM"),
+# (8,  "In treno o in macchina?",    "https://youtu.be/nmtMdxkKHUo"),
+# (9,  "Mi piace moltissimo!",       "https://youtu.be/7RXiaCyz7O8"),
+# (10, "Il concerto è andato bene!", "https://youtu.be/FpOEN93LX-E"),  
+# (11, "Ieri sera",                  "https://youtu.be/9A5gTgcgQ-I"),
 
 ]
 
